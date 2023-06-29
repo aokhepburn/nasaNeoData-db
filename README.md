7a7993eadd218b74f0f163f1c4c01652bf58f2ca
Alice Hepburn <aliceokhepburn@gmail.com>
2023-06-29 09:21:41 -0400

    UPDATE readme stylesheet -index.js still not reading the day-drop-down-menu when asked. HTML update with dropdown list no form no buttons

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 I am using the NASA Near Earth Object Web Service API. Right now I have copy pasted seven arrays that have between nine and twenty two objects representing asteroids to a locally hosted JSON file so I can practice with nested information. Each object is populated with information that the website is supposed to make it easier to sort through. The fields I am thinking of using so far are “was it potentially hazardous”, the size, the speed, how close it passed to Earth, and the date. For my event listeners I am thinking of drop down menus which I believe would be a click; a hover event that would result in a rollover with detailed information; and a form submit for naming one of the objects which would then be patched to the database.
 
 JAVASCRIPT
-    Maybe a form to select the specific day, each near miss would show up on a different size gray circle
-    (maybe make an array of random asteroid images for the text to appear on)
-    Use a comparison for scale for what each would show up
+    A form to select the specific day, each near miss would show up on a different size gray circle
+    -(maybe make an array of random asteroid images for the text to appear on)
+    -wse a comparison for scale for which would show up
 
     if is_potentially_hazardous_asteroid = true
     display estimated_diameter.kilometers
@@ -11,26 +11,19 @@ JAVASCRIPT
                         close_approach_data[i].miss_distance.kilometers
 
 
+    Main goal should be for users to easily interact with the data and choose how it is displayed.
+
+
 
 LAYOUT
--100% span nav bar
+-100% span header on alt colour
 -asteroids show up on scaled asteroid pngs
     -stored in seperate array
     -scaled: array of images reflective of relative estimated_diameter.kilometers.estimated_diameter_max
         -collect range of estimated_diameter.kilometers.estimated_diameter_max sizes and break into 5
--drop down menus - if hazardous the information can be sorted by
-    filter by: date
+-drop down menus - if hazardous  + the information can be sorted by
     filter by: size
     filter by: speed
     sort by: distance from earth
 -each result will display collection of asteroids that match true and be sorted
 
-AFTER LUNCH
- - set up nav bar div
- -set up first info div that will return a list
- -write function that will populate the list
-    -will populate the list from one of the sorted functions and will be identified with the corresponding name of each asteroid?
-
-
-TO DO
-    -FILTER HAZARDOUS RESULT
--- asteroidsIndex.js
+++ asteroidsIndex.js
@@ -1,61 +1,121 @@
 //TO DO PRINTSSORTEDLISTS FUNCTION NEEDS TO BE TURNED TO DETAILS POPULATE
 //PRINTSSORTEDLISTS WILL BE THE LESS DETAILED VERSION ROLLOVER CALLBACK WILL CALL DETAILS
 
-let dateKey
-
+let dateKey = '2023-01-01';
+let data;
+let asteroidWeekData
 //fetch for January First - January Seventh
-fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=${NASA_AUTH_KEY}`)
-    .then(res => res.json())
-    .then(data => {
-        dateKey = data.near_earth_objects
-        const sortedBySizes = sortsBySizes(data.near_earth_objects)
-                sortedBySizes.map(printsDetailedLists)
-        //printsSortedLists should be called on an array of hazardous asteroids as default load
+fetch(
+    `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=${NASA_AUTH_KEY}`
+)
+    .then((res) => res.json())
+    .then((data) => {
+        //isPotentiallyHazardousWithDateAndSize(data.near_earth_objects)
+        enablesSortingDropDownMenu(data.near_earth_objects[`${dateKey}`])
+        asteroidWeekData = data.near_earth_objects
+        //buttonEventListeners(data.near_earth_objects)
+
         //event listner for form submits will lead to the fully populated body
+    });
+let currentAsteroid;
 
-        // const sortedBySizes = sortsBySizes(data.near_earth_objects)
-        // sortedBySizes.map(printsDetailedLists)
-        console.log('console.log: basic fetch working')
+//document.querySelector('#sorting-option-div').style.display = "none"
+//document.querySelector('#day-drop-down-menu').addEventListener('mouseover', ()=> document.querySelector('#sorting-option-div').style.display = "block")
 
-        
-        document.querySelector('#sorting-drop-down-menu').addEventListener('change', (e)=>{
-            e.preventDefault()
-            console.log('hello from the event listener')
-            
-            //console.log(sortsBySizes(data.near_earth_objects))
-
-            //only getting two elements returned in console.log in first if statement when I just call sortsBySizes, if I take the retun
-            //out of the sortsBySizes functions i can console.log all elements so wtf
-
-            //functions are working if statements are not
-            
-            if (e.target.value === 'arrangeBySize'){
-                console.log('hello from the event listener')
-                const sortedBySizes = sortsBySizes(data.near_earth_objects)
-                //having two random asteroids  show up on console log - wait are they from my sorting?
-                sortedBySizes.map(printsDetailedLists)
-                console.log(sortedBySizes)
-            } else if (e.target.value ==='arrangeBySpeed'){
-                const sortedByVelocity = sortsByVelocity(data.near_earth_objects)
-                sortedByVelocity.forEach(printsDetailedLists)
-                console.log(sortedByVelocity)
-            } else {
-                const sortedByDistance = sortsByDistance(data.near_earth_objects)
-                sortedByDistance.forEach(printsDetailedLists)
-                console.log(sortedByDistance)
-            }
-        })
-        
-    })
-
-let currentAsteroid
 
 //first function: returns only those data points that return as true to hazardous and are identified with their date & size
-function isPotentiallyHazardousWithDateAndSize(data) {
-    for (let key of Object.keys(data)) {
-        for (let i = 0; i < data[key].length; i++) {
-            if (data[key][i].is_potentially_hazardous_asteroid == true) {
-                console.log(`(${data[key][i].name}) potentially hazardous: ${data[key][i].is_potentially_hazardous_asteroid}`)
+//POPULATES BODY
+
+let hazardousAsteroids
+// function dayDropDownMenuFunction(dataForEachDayMenu) {
+//     document.querySelector('#day-drop-down-menu').addEventListener('submit', (e) => {
+//         e.preventDefault()
+//         console.log(e.target)
+//         if (e.target.input.value = 'hazardfunction') { console.log('hazardfunction result') }
+//         else {
+//             console.assert(false)
+//             document.querySelector("#asteroid-list").innerHTML = ""
+//             dateKey = e.target.input.value
+//             const initialLoad = dataForEachDayMenu[`${keyDate}`]
+//             initialLoad.forEach(printsDetailedLists)
+//         }
+//     e.target.reset()
+//     })
+// }
+function buttonEventListeners(dataForButtons) {
+    document.querySelector('#day1week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.assert(false)
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+    })
+
+    document.querySelector('#day2week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.log('Button 2')
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+        console.log(sortedBySizes)
+    })
+
+    document.querySelector('#day3week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.assert(false)
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+    })
+
+    document.querySelector('#day4week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.assert(false)
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+    })
+
+    document.querySelector('#day5week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.assert(false)
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+    })
+    document.querySelector('#day6week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.assert(false)
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+    })
+    document.querySelector('#day7week1').addEventListener('click', (e) => {
+        document.querySelector("#asteroid-list").innerHTML = ""
+        console.assert(false)
+        dateKey = e.target.value
+        const sortedBySizes = sortsBySizes(dataForButtons[`${dateKey}`]);
+        sortedBySizes.forEach(printsDetailedLists)
+        enablesSortingDropDownMenu(dataForButtons[`${dateKey}`])
+    })
+}
+
+
+
+function isPotentiallyHazardousWithDateAndSize(dataForHazardous) {
+    for (let key of Object.keys(dataForHazardous)) {
+        for (let i = 0; i < dataForHazardous[key].length; i++) {
+            if (dataForHazardous[key][i].is_potentially_hazardous_asteroid == true) {
+                hazardousAsteroids = dataForHazardous[key][i]
+
+                printsDetailedLists(hazardousAsteroids)
+                //console.log(`(${data[key][i].name}) potentially hazardous: ${hazardousAsteroids.is_potentially_hazardous_asteroid}`)
             }
         }
     }
@@ -67,91 +127,107 @@ function isPotentiallyHazardousWithDateAndSize(data) {
 //have the date key be a second parameter so that each choice of date would vary
 //or have retrieve functions return new arrays that are than sorted through & printed
 
-function sortsByDistance(data) {
-    for (let i = 0; i < data['2023-01-01'].length; i++) {
-        let sortedByDistance = data['2023-01-01'].sort((a, b) => parseFloat(a.close_approach_data['0'].miss_distance.astronomical) - parseFloat(b.close_approach_data['0'].miss_distance.astronomical))
-        console.log(`(${sortedByDistance[i].name}) distance missed earth by: ${sortedByDistance[i].close_approach_data['0'].miss_distance.astronomical}`)
-        return sortedByDistance
+function sortsByDistance(dataForDistance) {
+    for (let i = 0; i < dataForDistance.length; i++) {
+        let sortedByDistance = dataForDistance.sort(
+            (a, b) =>
+                parseFloat(a.close_approach_data["0"].miss_distance.astronomical) -
+                parseFloat(b.close_approach_data["0"].miss_distance.astronomical)
+        );
+        // console.log(
+        //     `(${sortedByDistance[i].name}) distance missed earth by: ${sortedByDistance[i].close_approach_data["0"].miss_distance.astronomical}`
+        // );
+        return sortedByDistance;
     }
-
 }
 
-function sortsByVelocity(data) {
-    for (let i = 0; i < data['2023-01-01'].length; i++) {
-        let sortedBySpeed = data['2023-01-01'].sort((a, b) => parseFloat(a.close_approach_data['0'].relative_velocity.kilometers_per_second) - parseFloat(b.close_approach_data['0'].relative_velocity.kilometers_per_second))
-        console.log(`(${sortedBySpeed[i].name}) approach velocity: ${sortedBySpeed[i].close_approach_data['0'].relative_velocity.kilometers_per_second}`)
-        return sortedBySpeed
+function sortsByVelocity(dataForSpeed) {
+    for (let i = 0; i < dataForSpeed.length; i++) {
+        let sortedBySpeed = dataForSpeed.sort(
+            (a, b) =>
+                parseFloat(
+                    a.close_approach_data["0"].relative_velocity.kilometers_per_second
+                ) -
+                parseFloat(
+                    b.close_approach_data["0"].relative_velocity.kilometers_per_second
+                )
+        );
+        // console.log(
+        //     `(${sortedBySpeed[i].name}) approach velocity: ${sortedBySpeed[i].close_approach_data["0"].relative_velocity.kilometers_per_second}`
+        // );
+        return sortedBySpeed;
     }
-
 }
 
-function sortsBySizes(data) {
-    // let sortedBySize = data['2023-01-01'].sort((a, b) => parseFloat(a.estimated_diameter.kilometers.estimated_diameter_max) - parseFloat(b.estimated_diameter.kilometers.estimated_diameter_max))
-    // return sortedBySize
-    let sortedBySize = data['2023-01-01'].sort((a, b) => parseFloat(a.estimated_diameter.kilometers.estimated_diameter_max) - parseFloat(b.estimated_diameter.kilometers.estimated_diameter_max))
-    // for (let i = 0; i < data['2023-01-01'].length; i++) {
-    //     console.log(`(${sortedBySize[i].name}) estimated diameter: ${sortedBySize[i].estimated_diameter.kilometers.estimated_diameter_max} kilometres`)   
-    // }
-    // console.log(sortedBySize)
-    return sortedBySize
+function sortsBySizes(dataForSize) {
+    //console.assert(dateKey != null, "dateKey is null!!")
+    for (let i = 0; i < dataForSize.length; i++) {
+        let sortedBySize = dataForSize.sort(
+            (a, b) =>
+                parseFloat(a.estimated_diameter.kilometers.estimated_diameter_max) -
+                parseFloat(b.estimated_diameter.kilometers.estimated_diameter_max)
+        );
+        // console.log(
+        //     `(${sortedBySize[i].name}) estimated diameter: ${sortedBySize[i].estimated_diameter.kilometers.estimated_diameter_max} kilometres`
+        // );
+        return sortedBySize;
+    }
 }
 
 function printsDetailedLists(dataPoint) {
-    currentAsteroid = dataPoint
+    currentAsteroid = dataPoint;
 
-    const asteroidFlexContainers = document.createElement('div')
-    console.log(document.querySelector("#asteroid-list"))
-    document.querySelector('#asteroid-list').append(asteroidFlexContainers)
+    const asteroidFlexContainers = document.createElement("div");
+    document.querySelector("#asteroid-list").append(asteroidFlexContainers);
 
-    const nameFieldTitle = document.createElement('h3')
-    nameFieldTitle.textContent = `Name Of Asteroid:`
-    nameFieldTitle.id = 'name-field-title'
-    asteroidFlexContainers.append(nameFieldTitle)
+    const nameFieldTitle = document.createElement("h3");
+    nameFieldTitle.textContent = `Name Of Asteroid:`;
+    nameFieldTitle.id = "name-field-title";
+    asteroidFlexContainers.append(nameFieldTitle);
 
-    const nameField = document.createElement('h1')
-    nameField.textContent = `${dataPoint.name}`
-    nameField.id = 'name-field'
-    asteroidFlexContainers.append(nameField)
+    const nameField = document.createElement("h1");
+    nameField.textContent = `${dataPoint.name}`;
+    nameField.id = "name-field";
+    asteroidFlexContainers.append(nameField);
 
-    const dateAndTimeField = document.createElement('p')
-    dateAndTimeField.textContent = `Date Passed By: ${dataPoint.close_approach_data[0].close_approach_date_full}`
-    dateAndTimeField.id = 'dateAndTimeField'
-    asteroidFlexContainers.append(dateAndTimeField)
+    const dateAndTimeField = document.createElement("p");
+    dateAndTimeField.textContent = `Date Passed By: ${dataPoint.close_approach_data[0].close_approach_date_full}`;
+    dateAndTimeField.id = "dateAndTimeField";
+    asteroidFlexContainers.append(dateAndTimeField);
 
-    const distanceField = document.createElement('p')
-    distanceField.textContent = `Distance Missed Earth By: ${dataPoint.close_approach_data[0].miss_distance.astronomical} astronomical units`
-    distanceField.id = 'distance-field'
-    asteroidFlexContainers.append(distanceField)
+    const distanceField = document.createElement("p");
+    distanceField.textContent = `Distance Missed Earth By: ${dataPoint.close_approach_data[0].miss_distance.astronomical} astronomical units`;
+    distanceField.id = "distance-field";
+    asteroidFlexContainers.append(distanceField);
 
-    const speedField = document.createElement('p')
-    speedField.textContent = `Velocity When Passing: ${dataPoint.close_approach_data[0].relative_velocity.kilometers_per_second} kilometres per second`
-    speedField.id = 'speed-field'
-    asteroidFlexContainers.append(speedField)
+    const speedField = document.createElement("p");
+    speedField.textContent = `Velocity When Passing: ${dataPoint.close_approach_data[0].relative_velocity.kilometers_per_second} kilometres per second`;
+    speedField.id = "speed-field";
+    asteroidFlexContainers.append(speedField);
 
-    const sizeField = document.createElement('p')
-    sizeField.textContent = `Estimated Diameter In: ${dataPoint.estimated_diameter.kilometers.estimated_diameter_max} kilometres`
-    sizeField.id = 'size-field'
-    asteroidFlexContainers.append(sizeField)
+    const sizeField = document.createElement("p");
+    sizeField.textContent = `Estimated Diameter In: ${dataPoint.estimated_diameter.kilometers.estimated_diameter_max} kilometres`;
+    sizeField.id = "size-field";
+    asteroidFlexContainers.append(sizeField);
 
-    const hazardFieldTitle = document.createElement('p')
-    hazardFieldTitle.textContent = "Classified as Potentially Hazardous Asteroid"
-    asteroidFlexContainers.append(hazardFieldTitle)
+    const hazardFieldTitle = document.createElement("p");
+    hazardFieldTitle.textContent = "Classified as Potentially Hazardous Asteroid";
+    asteroidFlexContainers.append(hazardFieldTitle);
 
     if (dataPoint.is_potentially_hazardous_asteroid == true) {
-        const hazardField = document.createElement('h3')
-        hazardField.textContent = "🦖 The end is nigh 🦕"
-        hazardField.id = 'hazard-returns-true'
-        hazardFieldTitle.insertAdjacentElement('beforeend', hazardField)
+        const hazardField = document.createElement("h3");
+        hazardField.textContent = "🦖 The end is nigh 🦕";
+        hazardField.id = "hazard-returns-true";
+        hazardFieldTitle.insertAdjacentElement("beforeend", hazardField);
     } else {
-        const hazardField = document.createElement('h3')
-        hazardField.textContent = 'Safe!'
-        hazardField.id = 'hazard-returns-false'
-        hazardFieldTitle.insertAdjacentElement('beforeend', hazardField)
+        const hazardField = document.createElement("h3");
+        hazardField.textContent = "Safe!";
+        hazardField.id = "hazard-returns-false";
+        hazardFieldTitle.insertAdjacentElement("beforeend", hazardField);
     }
 
     //assignAsteroidBackground(dataPoint)
 }
-
 
 /*
 //function assigns scaled asteroid image as background to details div element
@@ -167,10 +243,66 @@ function assignAsteroidBackground(dataPoint) {
 }
 */
 
+function enablesSortingDropDownMenu(dataForSortingDropDown) {
+    console.log("test")
+    console.assert(false)
+    document
+        .querySelector("#sorting-drop-down-menu")
+        .addEventListener("change", (e) => {
+            e.preventDefault();
 
+            if (e.target.value === "arrangeBySize") {
+                console.log("hello from the event listener arrange 1")
+                document.querySelector("#asteroid-list").innerHTML = "";
+                const sortedBySizes = sortsBySizes(dataForSortingDropDown);
+                //having two random asteroids  show up on console log - wait are they from my sorting?
+                sortedBySizes.forEach(printsDetailedLists);
+                //console.log(sortedBySizes);
+            } else if (e.target.value === "arrangeBySpeed") {
+                document.querySelector("#asteroid-list").innerHTML = "";
+                const sortedByVelocity = sortsByVelocity(dataForSortingDropDown);
+                sortedByVelocity.forEach(printsDetailedLists);
+                //console.log(sortedByVelocity);
+            } else {
+                document.querySelector("#asteroid-list").innerHTML = "";
+                const sortedByDistance = sortsByDistance(dataForSortingDropDown);
+                sortedByDistance.forEach(printsDetailedLists);
+                //console.log(sortedByDistance);
+            }
 
+            e.target.reset;
+        })
+}
 
 /*
 when sorting based off what matches will I have to return a new object that then will have the sort function
 called on it? or can i just link the functions? 
 */
+function enablesSortingDropDownMenu(data) {
+    document
+       .querySelector("#sorting-drop-down-menu")
+       .addEventListener("change", (e) => {
+         e.preventDefault();
+         console.log("hello from the event listener");
+ 
+         if (e.target.value === "arrangeBySize") {
+           document.querySelector("#asteroid-list").innerHTML = "";
+           const sortedBySizes = sortsBySizes(data);
+           //having two random asteroids  show up on console log - wait are they from my sorting?
+           sortedBySizes.map(printsDetailedLists);
+           console.log(sortedBySizes);
+         } else if (e.target.value === "arrangeBySpeed") {
+           document.querySelector("#asteroid-list").innerHTML = "";
+           const sortedByVelocity = sortsByVelocity(data);
+           sortedByVelocity.forEach(printsDetailedLists);
+           console.log(sortedByVelocity);
+         } else {
+           document.querySelector("#asteroid-list").innerHTML = "";
+           const sortedByDistance = sortsByDistance(data);
+           sortedByDistance.forEach(printsDetailedLists);
+           console.log(sortedByDistance);
+         }
+ 
+         e.target.reset;
+       })
+     }
