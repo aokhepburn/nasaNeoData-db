01cbcd9fe3bd078f3c55cde4e746372ec9b12579
Alice Hepburn <aliceokhepburn@gmail.com>
2023-06-29 11:10:44 -0400

    UPDATE week 1 fully functioning

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 I am using the NASA Near Earth Object Web Service API. Right now I have copy pasted seven arrays that have between nine and twenty two objects representing asteroids to a locally hosted JSON file so I can practice with nested information. Each object is populated with information that the website is supposed to make it easier to sort through. The fields I am thinking of using so far are “was it potentially hazardous”, the size, the speed, how close it passed to Earth, and the date. For my event listeners I am thinking of drop down menus which I believe would be a click; a hover event that would result in a rollover with detailed information; and a form submit for naming one of the objects which would then be patched to the database.
 
+The database produces one json file for the max of a seven day range, at the moment you can only access pre-selected weeks, I would however eventually like to change the list elements of which week to view to a form input where the dates would be a variable and the user could choose their own range. The day drop down menu would have to take it's values from the form input so am leaving that idea at the moment to produce a viable product.
+
 JAVASCRIPT
     A form to select the specific day, each near miss would show up on a different size gray circle
     -(maybe make an array of random asteroid images for the text to appear on)
--- asteroidsIndex.js
+++ asteroidsIndex.js
@@ -1,58 +1,60 @@
-//TO DO PRINTSSORTEDLISTS FUNCTION NEEDS TO BE TURNED TO DETAILS POPULATE
-//PRINTSSORTEDLISTS WILL BE THE LESS DETAILED VERSION ROLLOVER CALLBACK WILL CALL DETAILS
+//PRINTS TITLES ONLY
 
 let dateKey = '2023-01-01';
 let asteroidWeekData
 //fetch for January First - January Seventh
-fetch(
-    `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=${NASA_AUTH_KEY}`
-)
-    .then((res) => res.json())
-    .then((data) => {
-        enableDayDropDownMenu(data.near_earth_objects)
-        asteroidWeekData = data.near_earth_objects
+document.querySelector('#each-day').style.display = "none"
 
-        //event listner for form submits will lead to the fully populated body
-    });
+document.querySelector('#week-1').addEventListener('click', (e) => {
+    fetch(
+        `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=${NASA_AUTH_KEY}`
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            enableDayDropDownMenu(data.near_earth_objects)
+            asteroidWeekData = data.near_earth_objects
+    
+            //event listner for form submits will lead to the fully populated body
+        })
+    
+        document.querySelector('#each-day').style.display = "block"
+
+
+})
+
 let currentAsteroid;
 
-//document.querySelector('#sorting-option-div').style.display = "none"
+document.querySelector('#sort-by').style.display = "none"
 //document.querySelector('#day-drop-down-menu').addEventListener('mouseover', ()=> document.querySelector('#sorting-option-div').style.display = "block")
 
 
-//first function: returns only those data points that return as true to hazardous and are identified with their date & size
-//POPULATES BODY
-
 let hazardousAsteroids
 
-//working as of 0945
 function enableDayDropDownMenu(dataForDayDropDown) {
     document.querySelector('#day-drop-down-menu').addEventListener('change', (e) => {
         e.preventDefault()
         if (e.target.value === 'hazardfunction') {
             document.querySelector("#asteroid-list").innerHTML = ""
-            isPotentiallyHazardousWithDateAndSize(dataForDayDropDown)
-            console.log("hello from the event listener dayHazard")
+            isPotentiallyHazardousPopulates(dataForDayDropDown)
+            document.querySelector('#sort-by').style.display = "none"
         }
         else {
             document.querySelector("#asteroid-list").innerHTML = ""
             dateKey = e.target.value
             const initialLoad = dataForDayDropDown[`${dateKey}`]
                   initialLoad.forEach(printsDetailedLists)
-            console.log(dateKey)
 
+            document.querySelector('#sort-by').style.display = "block"
+
+            enablesSortingDropDownMenu(dataForDayDropDown[`${dateKey}`])
         }
-        enablesSortingDropDownMenu(dataForDayDropDown[`${dateKey}`])
-        //e.reset()
-        // FUNCTION THAT LOADS APPROPRIATE DATA (?STORES OPTION AS A VALUE TO BE USED BY FOLLOWING LISTENER?) datekey?
-        // (FETCH WITHIN IF OPTIONS? W APPROPRIATE FUNCTIONS ON SAME PAGE CAN JUST CALL THEM IN THERE? -FOLLOWING EVENT LISTENER CALLED BY FETCH?)
     })
 }
 
 
-
-
-function isPotentiallyHazardousWithDateAndSize(dataForHazardous) {
+//returns only those data points that return as true to hazardous and are identified with their date & size
+//POPULATES BODY
+function isPotentiallyHazardousPopulates(dataForHazardous) {
     for (let key of Object.keys(dataForHazardous)) {
         for (let i = 0; i < dataForHazardous[key].length; i++) {
             if (dataForHazardous[key][i].is_potentially_hazardous_asteroid == true) {
