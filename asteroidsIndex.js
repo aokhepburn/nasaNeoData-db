170ab3e9970ceaa16472ba874614fd18c615a8c7
Alice Hepburn <aliceokhepburn@gmail.com>
2023-06-29 13:50:53 -0400

    UPDATE user-date-range drop downs added. all drop downs functional.

--- asteroidsIndex.js
+++ asteroidsIndex.js
@@ -2,36 +2,160 @@
 
 let dateKey = '2023-01-01';
 let asteroidWeekData
-//fetch for January First - January Seventh
-document.querySelector('#each-day').style.display = "none"
 
-document.querySelector('#week-1').addEventListener('click', (e) => {
+// document.querySelector('#each-day-week-1').style.display = "none"
+// document.querySelector('#each-day-week-2').style.display = "none"
+document.querySelector('#sort-by').style.display = "none"
+document.querySelector('#each-day').style.display = 'none'
+
+//working on range input form
+fetch('http://localhost:3000/dates')
+.then(res=>res.json())
+.then(data=>{ 
+    data.dayValues.forEach(addsStartDayOptions)
+    data.monthValues.forEach(addsStartMonthOptions)
+    data.dayValues.forEach(addsEndDayOptions)
+    data.monthValues.forEach(addsEndMonthOptions)
+    submitsDateEnablesMenu()
+
+    })
+let dayDateStart
+let monthDateStart
+let dayDateEnd
+let monthDateEnd
+let dateRange
+
+//populate day input from json - validation function
+function addsStartDayOptions(data){
+    const dayStartInputValue = document.createElement('option')
+    dayStartInputValue.innerText = data
+    dayStartInputValue.value = data
+    document.querySelector('#set-day-start').append(dayStartInputValue)
+}
+function addsStartMonthOptions(data){
+    const monthStartInputValue = document.createElement('option')
+    monthStartInputValue.innerText = data
+    monthStartInputValue.value = data
+    document.querySelector('#set-month-start').append(monthStartInputValue)
+    //console.log(data)
+}
+function addsEndDayOptions(data){
+    const dayEndInputValue = document.createElement('option')
+    dayEndInputValue.innerText = data
+    dayEndInputValue.value = data
+    document.querySelector('#set-day-end').append(dayEndInputValue)
+}
+function addsEndMonthOptions(data){
+    const monthEndInputValue = document.createElement('option')
+    monthEndInputValue.innerText = data
+    monthEndInputValue.value = data
+    document.querySelector('#set-month-end').append(monthEndInputValue)
+    //console.log(data)
+}
+
+//submit event
+//inputs dates from set-db-range form
+//uses them to fetch that set of data
+//returns those dates as part of a new array - dateRange
+function submitsDateEnablesMenu(){document.querySelector('#set-db-range').addEventListener('submit', (e) => {
+    e.preventDefault()
+    dayDateStart = e.target['set-day-start'].value
+    monthDateStart = e.target['set-month-start'].value
+    dayDateEnd = e.target['set-day-end'].value
+    monthDateEnd = e.target['set-month-end'].value
     fetch(
-        `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=${NASA_AUTH_KEY}`
+        `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-${monthDateStart}-${dayDateStart}&end_date=2023-${monthDateEnd}-${dayDateEnd}&api_key=${NASA_AUTH_KEY}`
     )
         .then((res) => res.json())
         .then((data) => {
-            enableDayDropDownMenu(data.near_earth_objects)
-            asteroidWeekData = data.near_earth_objects
-    
-            //event listner for form submits will lead to the fully populated body
+            dateRange = Object.keys(data.near_earth_objects)
+            dateRange.forEach(populatesDayOptions)
+            isPotentiallyHazardousPopulates(data.near_earth_objects)
+            enablesDayDropDown(data.near_earth_objects)
         })
     
-        document.querySelector('#each-day').style.display = "block"
+    document.querySelector('#each-day').style.display = 'block'
+    
+    e.target.reset()})}
+
+//dateRange has to be used to populate specific day options dateRange.forEach
+//and input into the correct value
+
+function populatesDayOptions(userDateRange){
+    const eachDayData = document.createElement('option')
+    eachDayData.innerText = userDateRange
+    eachDayData.value = userDateRange
+    document.querySelector('#day-drop-down-menu').append(eachDayData)
+}
+
+function enablesDayDropDown(dataForDayDropDown) {
+    document.querySelector('#day-drop-down-menu').addEventListener('change', (e) => {
+        e.preventDefault()
+        if (e.target.value === 'hazardfunction') {console.log(hazard)
+            document.querySelector("#asteroid-list").innerHTML = ""
+            isPotentiallyHazardousPopulates(dataForDayDropDown)
+            document.querySelector('#sort-by').style.display = "none"
+        }
+        else {
+            document.querySelector("#asteroid-list").innerHTML = ""
+            dateKey = e.target.value
+            const initialLoad = dataForDayDropDown[`${dateKey}`]
+                  initialLoad.forEach(printsDetailedLists)
+
+            document.querySelector('#sort-by').style.display = "block"
+
+            enablesSortingDropDownMenu(dataForDayDropDown[`${dateKey}`])
+        }
+    })
+}
+
+// //fetch for January First - January Seventh
+// document.querySelector('#week-1').addEventListener('click', (e) => {
+//     fetch(
+//         `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=${NASA_AUTH_KEY}`
+//     )
+//         .then((res) => res.json())
+//         .then((data) => {
+//             enableDayDropDownMenuWeek1(data.near_earth_objects)
+//             asteroidWeekData = data.near_earth_objects
+    
+//             //event listner for form submits will lead to the fully populated body
+//         })
+    
+//         document.querySelector('#each-day-week-1').style.display = "block"
 
 
-})
+// })
 
 let currentAsteroid;
-
-document.querySelector('#sort-by').style.display = "none"
 //document.querySelector('#day-drop-down-menu').addEventListener('mouseover', ()=> document.querySelector('#sorting-option-div').style.display = "block")
 
 
 let hazardousAsteroids
 
-function enableDayDropDownMenu(dataForDayDropDown) {
-    document.querySelector('#day-drop-down-menu').addEventListener('change', (e) => {
+function enableDayDropDown(dataForDayDropDown) {
+    document.querySelector('#day-drop-down-menu-week-1').addEventListener('change', (e) => {
+        e.preventDefault()
+        if (e.target.value === 'hazardfunction') {
+            document.querySelector("#asteroid-list").innerHTML = ""
+            isPotentiallyHazardousPopulates(dataForDayDropDown)
+            document.querySelector('#sort-by').style.display = "none"
+        }
+        else {
+            document.querySelector("#asteroid-list").innerHTML = ""
+            dateKey = e.target.value
+            const initialLoad = dataForDayDropDown[`${dateKey}`]
+                  initialLoad.forEach(printsDetailedLists)
+
+            document.querySelector('#sort-by').style.display = "block"
+
+            enablesSortingDropDownMenu(dataForDayDropDown[`${dateKey}`])
+        }
+    })
+}
+
+function enableDayDropDownMenuWeek2(dataForDayDropDown) {
+    document.querySelector('#day-drop-down-menu-week-2').addEventListener('change', (e) => {
         e.preventDefault()
         if (e.target.value === 'hazardfunction') {
             document.querySelector("#asteroid-list").innerHTML = ""
